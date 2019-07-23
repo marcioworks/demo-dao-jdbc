1a31aefa5bc4575e7b7ed5ba92cba619ec7268b2
silvamarcio <silva.marcio86@hotmail.com>
2019-07-22 22:24:03 -0300

    Department Delete

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -11,20 +11,25 @@ public class Program2 {
 		
 		DepartmentDao depDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("=======TEST 1: Department Insert ");
+		/*System.out.println("=======TEST 1: Department Insert ");
 		Department dep = new Department(null,"Books");
 		depDao.insert(dep);
 		System.out.println("Insert completed id = "+ dep.getId());
-		
+	
 		System.out.println("=======TEST 2: Department find By ID ");
 		Department dep2 = depDao.FindById(10);
 		System.out.println(dep2);
 		
 		System.out.println("=======TEST 3: Department Update ");
-		dep2 = depDao.FindById(9);
-		dep2.setName("Eletronics");
+		dep2 = depDao.FindById(3);
+		dep2.setName("Food");
 		depDao.update(dep2);
 		System.out.println("update complete!");
+			*/
+		
+		System.out.println("=======TEST 4: Department DELETE ");
+		depDao.deleteById(12);
+		System.out.println("DELETE completed");
 		
 	}
 
